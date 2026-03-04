5c8430aaf3c6979ac5634536ff1343cec0214770
lsirikh <lsirikh@naver.com>
2026-03-04 20:44:33 +0900

    updated for notifying to DB monitor bug fix2

--- app/config.py
+++ app/config.py
@@ -14,6 +14,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         case_sensitive=True,
+        extra="ignore",
     )
 
     # Authentication
